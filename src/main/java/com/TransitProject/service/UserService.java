17dea07d527afdeeeaf84437dc1f29bcb1293ca4
Jaskirat singh kochhar <kochharj@student.douglascollege.ca>
2019-11-16 22:37:02 -0800

    login and home page created

---
+++ src/main/java/com/TransitProject/service/UserService.java
@@ -0,0 +1,11 @@
+package com.TransitProject.service;
+
+import com.TransitProject.model.Login;
+import com.TransitProject.model.User;
+
+public interface UserService {
+
+  void register(User user);
+
+  User validateUser(Login login);
+}
