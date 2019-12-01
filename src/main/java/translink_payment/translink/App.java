1c08e7dec2a96245daf5e620e90df2c637d11213
nikso <nikso@LAPTOP-7655EPR3>
2019-12-01 15:25:53 +0530

    Jetty + Maven Configuration

---
+++ src/main/java/translink_payment/translink/App.java
@@ -0,0 +1,13 @@
+package translink_payment.translink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
